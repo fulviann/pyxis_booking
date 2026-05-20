297dd05bcf3aaeea28df1c595c95abb3e703d4b0
fulviann <rayhanfulvian@gmail.com>
2026-05-20 17:49:03 +0700

    fix sql

--- back-end/migrations/000000_create_pyxis_table.down.sql
+++ back-end/migrations/000000_create_pyxis_table.down.sql
@@ -1,3 +1,7 @@
+-- ======================
+-- DROP CHILD TABLES FIRST
+-- ======================
+
 DROP TABLE IF EXISTS webhook_logs;
 DROP TABLE IF EXISTS invalid_token;
 
@@ -7,19 +11,32 @@ DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS blackouts;
 
 DROP TABLE IF EXISTS services;
-DROP TABLE IF EXISTS reserv_h;
 
 DROP TABLE IF EXISTS merchants;
 
-DROP TABLE IF EXISTS subscription_payments;
+DROP TABLE IF EXISTS subscription_payment;
+
+DROP TABLE IF EXISTS reserv_h;
+
 DROP TABLE IF EXISTS transactions;
 
+DROP TABLE IF EXISTS subscription_list;
+
+-- ======================
+-- DROP MASTER TABLES
+-- ======================
+
+DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS admin;
 DROP TABLE IF EXISTS owner;
-DROP TABLE IF EXISTS customer;
 
 DROP TABLE IF EXISTS packages;
 DROP TABLE IF EXISTS service_category;
-DROP TABLE IF EXISTS subscription_list;
+
+DROP TABLE IF EXISTS master_sequence;
+
+-- ======================
+-- DROP ENUM
+-- ======================
 
 DROP TYPE IF EXISTS webhook_status;
